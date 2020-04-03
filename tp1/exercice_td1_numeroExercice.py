68247cd7e511c2112e89a5c0c1fcc156e765a45a
Gmirmand <gmirmand@webqam.fr>
2020-04-03 14:54:25 +0200

    push ex 2

--- tp1/exercice_td1_numeroExercice.py
+++ tp1/exercice_td1_numeroExercice.py
@@ -6,3 +6,11 @@ carre = 0
 valeur = int(input("saisir un entier: "))
 carre = valeur**2
 print(carre)
+print(type(carre))
+
+"""
+ex2
+"""
+print("Boucle Pour:")
+for i in range(0, 21, 2):  # pour i allant de 0 à 20 par pas de 2
+    print(f"--> i vaut: {i}")  # je repète l'action 20 fois
