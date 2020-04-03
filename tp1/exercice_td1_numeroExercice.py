26429ca146c8c6b167da7794796fe39e27132a38
Gmirmand <gmirmand@webqam.fr>
2020-04-03 15:45:36 +0200

    push ex 5

--- tp1/exercice_td1_numeroExercice.py
+++ tp1/exercice_td1_numeroExercice.py
@@ -37,3 +37,26 @@ if 1 < valeur < 100:
 else:
     for i in range(1, valeur+1):
         print(f"--> i vaut : {i}")
+
+
+"""
+ex5
+"""
+print("========= ex5 ==========")
+nb1 = int(input("Donnez un premier nombre : "))
+nb2 = int(input("Donnez un deuxieme nombre : "))
+symbol = input("Donnez le symbole de l'operation (+, -, *, /) : ")
+
+if symbol == '*':
+    result = nb1 * nb2
+elif symbol == '+':
+    result = nb1 + nb2
+elif symbol == '/':
+    result = nb1 / nb2
+elif symbol == '*':
+    result = nb1 * nb2
+else:
+    print(f"le symbole {symbol} n'est pas valide")
+
+if result:
+    print(f"Le résultat est : {result}")
