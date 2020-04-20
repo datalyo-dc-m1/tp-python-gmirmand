13af57f2725b05525a264c415381a8ba06408718
Gmirmand <gmirmand@webqam.fr>
2020-04-20 14:52:39 +0200

    Fix len

--- tp3/listes.py
+++ tp3/listes.py
@@ -9,6 +9,7 @@ noteMin = min(grades)
 print('Il y a un écart de ', noteMax - noteMin, 'entre la meilleur et la pire note')
 
 grades.append(14)
+nbEleve = len(grades)
 print('La note de 14 a été ajoutée', grades)
 
 grades[4] = 13
@@ -17,6 +18,7 @@ print('La 5e note a été changée en 13', grades)
 moyenne = sum(grades) / nbEleve
 print('La moyenne de la classe est de', moyenne)
 
+# Utiliser sorted() ou faire un grades.sort() avant
 median = sorted(grades)[int(nbEleve/2)]
 print('La médiane est', median)
 
