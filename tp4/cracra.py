b3770b4fbd02dd2a9d143c498edfbd4c2476c936
Gmirmand <gmirmand@webqam.fr>
2020-04-20 16:01:29 +0200

    Reformat cracra

--- tp4/cracra.py
+++ tp4/cracra.py
@@ -1,29 +1,33 @@
 import unittest
 
 class Fonctions:
-    def   Fonction (self, var ):
-            if type (var) != int :
-                return f"{var} {fonction()}"
-            elif    var <= -1  :
-             return f"Il est {var} heure "
-            elif  var >= 25:
-                   return f"{var} nest pas 1 chiffre"
+    def Fonction(self, var):
+        if type(var) != int:
+            return f"{var} {fonction()}"
+        elif var <= -1:
+            return f"Il est {var} heure "
+        elif var >= 25:
+            return f"{var} nest pas 1 chiffre"
+        else:
+            if var < 12:
+                return f"Il est {var}HOO, c'est le matin"
             else:
-                if var < 12:
-                 return f"Il est {var}HOO, c'est le matin"
+                if var >= 18:
+                    return f"Il est {var}H00, c'est le soir"
+                elif var > 12 and var <= 18:
+                    return f"Il est {var}h, c'est l'après-midi "
                 else:
-                 if var >= 18:
-                  return f"Il est {var}H00, c'est le soir"
-                 elif var > 12 and var <= 18 :
-                      return f"Il est {var}h, c'est l'après-midi "
-                 else     :
-                  return f"{var} {fonction()}"
+                    return f"{var} {fonction()}"
+
 
 def fonction():
     return "n'est pas un chiffre"
+
+
 int = 3
 
 print(Fonctions().Fonction(3))
+
 
 class UnitTest(unittest.TestCase):
     def test_Fonction(self):
